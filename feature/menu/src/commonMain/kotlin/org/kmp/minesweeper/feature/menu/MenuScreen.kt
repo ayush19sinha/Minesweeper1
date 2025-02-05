7927768b99f462b532a95f065de63e29279e720d
ayushsinha-1 <143383811+ayushsinha-1@users.noreply.github.com>
2025-02-05 20:48:07 +0530

    Added global dimensions and paddings

--- feature/menu/src/commonMain/kotlin/org/kmp/minesweeper/feature/menu/MenuScreen.kt
+++ feature/menu/src/commonMain/kotlin/org/kmp/minesweeper/feature/menu/MenuScreen.kt
@@ -3,9 +3,13 @@ package org.kmp.minesweeper.feature.menu
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.kmp.minesweeper.ui.core.LocalDimension
+import org.kmp.minesweeper.ui.core.LocalPadding
 
 @Composable
 internal fun MenuScreen(
@@ -20,6 +24,8 @@ internal fun MenuScreen(
         modifier = modifier,
     ) {
         val buttonModifier = Modifier
+            .padding(horizontal = LocalPadding.current.normal)
+            .widthIn(max = LocalDimension.current.maxWidthSmall)
             .fillMaxWidth()
         MenuButton(
             text = "Play",
